9742caae647f0490a19cfdea67fc87a15687a51d
Tinkjimmy <tinkjimmy938@gmail.com>
2024-01-21 21:29:45 +0100

    task 2.6

---
+++ src/recipe_project/views.py
@@ -0,0 +1,31 @@
+
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import redirect, render
+
+def login_view(request):
+    form = AuthenticationForm()
+    error_message = None  # Initialize error_message with a default value
+
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('recipes:list')
+
+            else:
+                error_message = 'Invalid username or password'
+
+    context = {
+        'form': form,
+        'error_message': error_message,
+    }
+    return render(request, 'auth/login.html', context)
+
+def logout_view(request):
+    logout(request) #pre-defined logout django function
+    return render(request, 'auth/success.html')
--- src/recipes/views.py
+++ src/recipes/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.views.generic import ListView,DetailView
 from.models import Recipe
+from django.contrib.auth.mixins import LoginRequiredMixin #this libary is required to protect a class-based view
 
 # Create your views here.
+ 
 def home(request):
     return render(request,'recipes/recipes_home.html')
 
-class RecipeListView(ListView):          
+class RecipeListView(LoginRequiredMixin,ListView):          
    model = Recipe                        
    template_name = 'recipes/main.html' 
 
